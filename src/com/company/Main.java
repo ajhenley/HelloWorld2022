1bdf32372283b8e1bd73ab99c2fba2e9dfd3ef9e
Alton Henley <altonhenley@Altons-Mac-mini.local>
2022-01-24 16:12:58 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int x = 5; // declaring and initializing
+        int y;
+        String s;
+        s = "Java";
+        boolean b;
+        b = false;
+        double dbl;
+        dbl = 3.14;
+        y = 2;
+        System.out.print("Hello Class");
+        System.out.print("Hello Class");
+        System.out.println("Hello Class");
+        System.out.println(x);
+        System.out.println(y);
+    }
+}
